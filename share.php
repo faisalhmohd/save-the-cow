38c548e6419487abf77c927f3966b8eeba46c661
Mohammad Faisal <faisalhmohd@live.com>
2015-09-21 20:03:11 +0600

    Finalized Version

--- share.php
+++ share.php
@@ -5,10 +5,10 @@ $score = $_GET["score"];
 <html>
 <head>
 <title>Share Your Score | The Ruchi Game </title>
-<meta property="og:title" content="I have just scored <?php echo $score;?>!"/>
+<meta property="og:title" content="I have scored <?php echo $score;?>!"/>
 <meta property="og:image" content="http://theruchigame.com/images/sharepic.png"/>
 <meta property="og:site_name" content="The Ruchi Game"/>
-<meta property="og:description" content="<?php echo $score;?> is my new score on Save The Cow, try beating my new score! Also get a chance to win exciting prizes."/> 
+<meta property="og:description" content="This Eid, let's show our utmost love and affection to the animals. So, hop in and save thy cow! "/> 
 </head>
 <body>
 <meta http-equiv="refresh" content="0;URL=http://theruchigame.com" />
